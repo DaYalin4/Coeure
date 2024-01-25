cf2662f055038e69326f923488ec76d7befd14e7
DaYalin4 <99153869+DaYalin4@users.noreply.github.com>
2024-01-25 17:59:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-document.getElementById("inicio-btn").addEventListener("click", function() {
-    Sk.configure({output: drawOutput});
-    var code = `
-from turtle import *
-from colorsys import *
+document.addEventListener("DOMContentLoaded", function() {
+    var canvas = document.getElementById('turtleCanvas');
+    var ctx = canvas.getContext('2d');
+    
+    var h = 0;
+    for (var i = 0; i < 371; i++) {
+        var c = hsv_to_rgb(h, 1, 1);
+        h += 0.005;
+        ctx.beginPath();
+        ctx.strokeStyle = 'rgb(' + Math.floor(c[0] * 255) + ',' + Math.floor(c[1] * 255) + ',' + Math.floor(c[2] * 255) + ')';
+        ctx.arc(canvas.width / 2, canvas.height / 2, -i * 0.68, 0, Math.PI * 2, true);
+        ctx.stroke();
+        ctx.closePath();
+        ctx.rotate(80 * Math.PI / 180);
+    }
 
-bgcolor('black')
-speed(0)
-h=0
-
-for i in range(371):
-    c=hsv_to_rgb(h,1,1)
-    h+=0.005
-    color(c)
-    circle(-i*0.68,200)
-    right(80)
-
-done()
-    `;
-    Sk.importMainWithBody("<stdin>", false, code);
+    function hsv_to_rgb(h, s, v) {
+        var i = Math.floor(h * 6);
+        var f = h * 6 - i;
+        var p = v * (1 - s);
+        var q = v * (1 - f * s);
+        var t = v * (1 - (1 - f) * s);
+        var mod = i % 6;
+        var r = [v, q, p, p, t, v][mod];
+        var g = [t, v, v, q, p, p][mod];
+        var b = [p, p, t, v, v, q][mod];
+        return [r, g, b];
+    }
 });
-
-function drawOutput(out) {
-    Sk.canvas = "turtleCanvas";
-    Sk.tg = Sk.TurtleGraphicsScraper();
-    Sk.runner.restart();
-}
-
