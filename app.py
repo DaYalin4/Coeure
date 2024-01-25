2d22af48b790acd6f1f7fd9a4a280344d4779665
DaYalin4 <99153869+DaYalin4@users.noreply.github.com>
2024-01-25 17:30:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/ejecutar_codigo')
+@app.route('/ejecutar_codigo', methods=['POST'])  # Cambiado a POST para enviar datos al servidor
 def ejecutar_codigo():
-    proceso = subprocess.Popen(['python', 'codigo_turtle.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    salida, error = proceso.communicate()
-    if error:
-        return f'Error al ejecutar el código: {error.decode()}'
-    else:
-        return 'Código Turtle ejecutado con éxito'
+    try:
+        proceso = subprocess.Popen(['python', 'codigo_turtle.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        salida, error = proceso.communicate()
+        if error:
+            return f'Error al ejecutar el código: {error.decode()}', 500  # Devolver error 500 si hay un error
+        else:
+            return 'Código Turtle ejecutado con éxito'
+    except Exception as e:
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
